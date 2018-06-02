bf535928b21610eeae82695f7b158cd6ad4030c3
Kevin Menegatti <kevin.menegatti@gmail.com>
2018-06-02 13:22:02 +0200

    Update cryptonight.c

--- cryptonight/cryptonight.c
+++ cryptonight/cryptonight.c
@@ -2,7 +2,7 @@
 #include "cryptonight/hash-ops.h"
 
 __declspec(dllexport) void cryptonight(char *input, int size, char *output, int variant, int prehashed)			// uncomment for windows, comment for linux (before compilation)
-//extern void cryptonight(char *input, int size, char *output, int variant, int prehashed)						// uncomment for linux, comment for windows (before compilation)
+//extern void cryptonight(char *input, int size, char *output, int variant, int prehashed)				// uncomment for linux, comment for windows (before compilation)
 {	
 	cn_slow_hash(input, size, output, variant, prehashed);
 }
